889528f69c44342b1f700d95b3526ec305eee8fd
patrick brisbin <pbrisbin@gmail.com>
2014-01-06 15:29:10 -0500

    Refactor Xrandr.Command make extendRight safer
    * Extract some more mid-level composed commands
    * Ensure displays without modes are skipped
    * Ensure --right-of is not added if no output was turned on

--- MSU/Xrandr/Command.hs
+++ MSU/Xrandr/Command.hs
@@ -7,39 +7,51 @@ import MSU.Display
 type Xrandr a = Writer String a
 
 output :: Display -> Xrandr ()
-output d = tell $ " --output " ++ name d
+output = tell . (" --output " ++) . name
 
 off :: Xrandr ()
 off = tell " --off"
 
 mode :: Mode -> Xrandr ()
-mode m = tell $ " --mode " ++ show m
+mode = tell . (" --mode " ++) . show
 
 rightOf :: Display -> Xrandr ()
-rightOf d = tell $ " --right-of " ++ name d
+rightOf = tell . (" --right-of " ++) . name
+
+outputOff :: Display -> Xrandr ()
+outputOff d = output d >> off
+
+-- | Does nothing but return @False@ if the output's not connected.
+outputOn :: Display -> Xrandr Bool
+outputOn (Display _ []) = return False
+outputOn d@(Display _ (m:_)) = output d >> mode m >> return True
+
+-- | Some Apple displays freak out if you change the resolution, turning
+--   it "off and then on again" seems to avoid that. Sigh.
+outputOffOn :: Display -> Xrandr Bool
+outputOffOn d = outputOff d >> outputOn d
 
 allOff :: [Display] -> Xrandr ()
-allOff = mapM_ (\d -> output d >> off)
+allOff = mapM_ outputOff
 
--- | N.B. Only to be called on connected displays
 firstOn :: [Display] -> Xrandr ()
-firstOn [] = return ()
-firstOn (display:_) = output display >> mode (head $ modes display)
+firstOn []    = return ()
+firstOn (d:_) = outputOn d >> return ()
 
--- | N.B. Only to be called on connected displays
 extendRight :: [Display] -> Xrandr ()
 extendRight [] = return ()
-extendRight (first:rest) = do
-    foldM_ setRight first rest
+extendRight (first:rest) = foldM_ setRight first rest
 
     where
         setRight :: Display -> Display -> Xrandr Display
         setRight primary secondary = do
-            output secondary >> off
-            output secondary >> mode (head $ modes secondary)
-            rightOf primary
+            outputOffOn secondary &&> rightOf primary
 
             return secondary
 
 buildCommand :: (Xrandr a) -> String
 buildCommand f = execWriter $ tell "xrandr" >> f
+
+-- | Executes the second action IFF the first returns @True@.
+(&&>) :: Monad m => m Bool -> m () -> m ()
+f &&> g = f >>= \b -> if b then g else return ()
